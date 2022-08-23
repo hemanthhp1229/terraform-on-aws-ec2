8af3b614fa60eac915706b9422ece207d50d88b4
EC2 Default User <ec2-user@ip-172-31-20-214.ec2.internal>
2022-08-23 14:39:38 +0000

    checking my upstream

--- 09-AWS-ALB-Application-LoadBalancer-Basic/terraform-manifests/c7-04-ec2instance-private.tf
+++ 09-AWS-ALB-Application-LoadBalancer-Basic/terraform-manifests/c7-04-ec2instance-private.tf
@@ -13,7 +13,6 @@ module "ec2_private" {
   vpc_security_group_ids = [module.private_sg.this_security_group_id]
   #subnet_id              = module.vpc.public_subnets[0]  
   subnet_id =module.vpc.private_subnets[0]
-#   instance_count         = var.private_instance_count
   user_data = file("${path.module}/app1-install.sh")
   tags = local.common_tags
 }
