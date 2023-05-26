014ef2a47d33cf341daec700c7f8de14ad7aa923
Hemanth Kumar M S <Hemanthhp1229@gmail.com>
2023-05-26 05:08:11 -0500

    Update ec2-instance.tf
    changed the ami id

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -16,6 +16,6 @@ provider "aws" {
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0cff7528ff583bf9a" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-0715c1897453cabd1" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
